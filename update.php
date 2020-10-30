5a7b80e80c0b5fab13ef0d1b38f00a26d6f3e105
Oleksandr Krokhin <alexander@krohin.com>
2020-10-30 16:05:47 +0200

    fix new version for update

--- update.php
+++ update.php
@@ -81,7 +81,7 @@ class PmaUpdater
 
             $obj = new stdClass();
             $obj->slug = $this->slug;
-            $obj->new_version = $this->githubAPIResult->tag_name;
+            $obj->version = $this->githubAPIResult->tag_name;
             $obj->url = $this->pluginData["PluginURI"];
             $obj->package = $package;
             $transient->response[$this->slug] = $obj;
